c048191d3a2e72f0b111a4de1eb5c4ae712127e8
Gordon Chung <chungg@ca.ibm.com>
2014-03-17 16:02:56 -0400

    add OpenStack Foundation as author
    add OpenStack Foundation as author
    
    Change-Id: Iac1b743fa4c0d0ead2badaee5f36451e91465bf0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyCADF'
-copyright = u'2014, gordon chung'
+copyright = u'2014, OpenStack Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -200,7 +200,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'pyCADF.tex', u'pyCADF Documentation',
-   u'gordon chung', 'manual'),
+   u'OpenStack Foundation', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,7 +230,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'pycadf', u'pyCADF Documentation',
-     [u'gordon chung'], 1)
+     [u'OpenStack Foundation'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -244,7 +244,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'pyCADF', u'pyCADF Documentation',
-   u'gordon chung', 'pyCADF', 'One line description of project.',
+   u'OpenStack Foundation', 'pyCADF', 'One line description of project.',
    'Miscellaneous'),
 ]
 
